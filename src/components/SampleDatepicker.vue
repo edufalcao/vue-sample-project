23e3b152a088bf4c88fabd962d5490726826e56f
Eduardo Falcão Lima <edu.falcao@gmail.com>
2023-07-24 20:47:56 -0300

    First commit

---
+++ src/components/SampleDatepicker.vue
@@ -0,0 +1,31 @@
+<script>
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+export default {
+  compatConfig: {
+        MODE: 3,
+        COMPONENT_ASYNC: false
+    },
+  components: {
+    VueDatePicker
+  },
+  data() {
+    return {
+      date: new Date(),
+    };
+  }
+}
+</script>
+
+<template>
+  <h1>Playground</h1>
+
+  <VueDatePicker
+  v-model:value="date"
+  format="YYYY-MM-DD"
+  type="date"
+  placeholder="Select date"
+  >
+  </VueDatePicker>
+</template>
